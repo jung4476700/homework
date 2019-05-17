208edea471a1f2594877cf7f377c6df7fed2eb48
jung4476700 <jung4476700@gmail.com>
2019-05-17 20:12:50 +0900

    jung

---
+++ myapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    update_date = models.DateTimeField(auto_now=True)
+    video_key=models.CharField(max_length=50, null=True)
+    
+
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.body[:50]
